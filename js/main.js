a347c06ec440dca39e1a16312bd9dfa6f855686e
Mohamed-Ibrahim-1 <mohamedibrahim633398@gmail.com>
2024-02-07 23:19:49 +0200

    01

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+
 let x = document.querySelector(".logo");
 x.onclick =function (){
     nav =document.querySelector(".content-area aside")
     nav.classList.toggle("actives");
     document.getElementById("navs").style.cssText="display: block";
     nav.classList.toggle('none');
+
+    section11 =document.querySelector(".right-section11")
+    section11.classList.toggle("actives");
+    document.getElementById("section11").style.cssText="display: flex";
+    section11.classList.toggle('none');
+
+
+    section1 =document.querySelector(".right-section")
+    section1.classList.toggle("actives");
+    document.getElementById("right-section1").style.cssText="display: flex";
+    section1.classList.toggle('none');
+
+
 }
 function dark (){
     document.body.style.cssText="background-color:#0000004a";
